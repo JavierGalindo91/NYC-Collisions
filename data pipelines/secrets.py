2065f02cf63465803323139de8d899ceda763fc5
Javier <javier.galindobrito@gmail.com>
2022-04-17 23:34:28 -0400

    Delete secrets.py

--- data pipelines/secrets.py
+++
@@ -1,8 +0,0 @@
-#Research .gitignore for this file 
-access_key = 'AKIARXLKH6RS6LYRYX45'
-secret_access_key ='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'
-app_token = 'ZKHXHy0D1PulHtoZaGrnj974v'
-
-arc_gis_user = 'javier.galindo91'
-arc_gis_pwd = 'F33lth3b34t!'
-arc_gis_APP_TOKEN = 'AAPK55d530f67c5e4c85bcfac4dee9c431c9mM7iCHTVnBcV9F_h20vn4wu4OhghsvR3DCIzaXNtdp5qQy_RbXtm8QETfFnJXKwV'
